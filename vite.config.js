eef72e81fc7d1fd986fe9505a27a2c578625c59f
Jaouadi Fadi <jawedifadi@gmail.com>
2026-06-27 16:35:49 +0100

    new feature added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Modifiez cette valeur si le nom de votre repository est différent.
+  // Par défaut, pour https://github.com/Jaouadi2004-Fedi/Jaouadi_Fadi.github.io
+  base: '/Jaouadi_Fadi.github.io/',
   plugins: [
     react(),
     tailwindcss(),
